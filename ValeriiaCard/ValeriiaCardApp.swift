3bc4f13531cc56cfdb2280ab322e05bab60f5c8b
ValeriaTishina <lyolik2038@gmail.com>
2021-04-02 23:10:15 +0400

    Initial Commit

---
+++ ValeriiaCard/ValeriiaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValeriiaCardApp.swift
+//  ValeriiaCard
+//
+//  Created by Валерия Тишина on 02.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct ValeriiaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
